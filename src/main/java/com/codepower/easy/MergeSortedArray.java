949f650a6e9e3b4817777fa4707328de4355c83f
justin <justinmamathew@gmail.com>
2024-01-25 06:39:45 +0530

    merge sorted array a simple approach

---
+++ src/main/java/com/codepower/easy/MergeSortedArray.java
@@ -0,0 +1,47 @@
+package com.codepower.easy;
+
+import javax.management.remote.JMXServerErrorException;
+import java.util.Arrays;
+
+/**
+ * @author Justin Mathew @dev_io
+ * A simple problem with a medium level solustion with three pointer.
+ *
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+ * To accommodate this, nums1 has a length of m + n,
+ * where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ */
+public class MergeSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums1={1,2,3,0,0,0};
+        int[] nums2={2,5,6};
+        merge(nums1,3, nums2,3);
+        System.out.println(nums1);
+    }
+
+    //The worst one but good to start..
+    public void mergeWorst(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Set p1 and p2 to point to the end of their respective arrays.
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        // And move p backward through the array, each time writing
+        // the smallest value pointed at by p1 or p2.
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (p2 < 0) {
+                break;
+            }
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--];
+            } else {
+                nums1[p] = nums2[p2--];
+            }
+        }
+    }}
